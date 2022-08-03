c390fad4fa964d2ceb1e000caf72fda4541f4aec
HilbertCurve <80780490+HilbertCurve@users.noreply.github.com>
2022-08-03 13:17:44 -0400

    minor changes

--- macros/target/debug/build/quote-1626857e7e6386b3/build_script_build-1626857e7e6386b3.d
+++
@@ -1,5 +0,0 @@
-/home/ben/rust/rage/macros/target/debug/build/quote-1626857e7e6386b3/build_script_build-1626857e7e6386b3: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs
-
-/home/ben/rust/rage/macros/target/debug/build/quote-1626857e7e6386b3/build_script_build-1626857e7e6386b3.d: /home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs
-
-/home/ben/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.21/build.rs:
--- macros/target/debug/deps/macros-c76d99ab0d2cbdfd.d
+++
@@ -1,6 +0,0 @@
-/home/ben/rust/rage/macros/target/debug/deps/macros-c76d99ab0d2cbdfd.rmeta: src/lib.rs src/component_derive.rs
-
-/home/ben/rust/rage/macros/target/debug/deps/macros-c76d99ab0d2cbdfd.d: src/lib.rs src/component_derive.rs
-
-src/lib.rs:
-src/component_derive.rs:
